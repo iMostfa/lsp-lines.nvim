c817abb5cb7b290df0bad627627b75319c88d0d9
iMostafa <mostafa.essam@wallapop.com>
2024-12-26 14:32:04 +0300

    autohide for current line if insert

--- lua/lsp_lines/init.lua
+++ lua/lsp_lines/init.lua
@@ -7,7 +7,8 @@ local function render_current_line(diagnostics, ns, bufnr, opts)
   local lnum = vim.api.nvim_win_get_cursor(0)[1] - 1
 
   for _, diagnostic in pairs(diagnostics) do
-    local show = diagnostic.end_lnum and (lnum >= diagnostic.lnum and lnum <= diagnostic.end_lnum) or (lnum == diagnostic.lnum)
+    local show = diagnostic.end_lnum and (lnum >= diagnostic.lnum and lnum <= diagnostic.end_lnum) or
+    (lnum == diagnostic.lnum)
     if show then table.insert(current_line_diag, diagnostic) end
   end
 
@@ -32,9 +33,9 @@ M.setup = function()
 
       vim.api.nvim_clear_autocmds({ group = "LspLines" })
       if opts.virtual_lines.only_current_line then
-        vim.api.nvim_create_autocmd("CursorMoved", {
+        vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
           buffer = bufnr,
-          callback = function ()
+          callback = function()
             render_current_line(diagnostics, ns.user_data.virt_lines_ns, bufnr, opts)
           end,
           group = "LspLines"
@@ -50,7 +51,7 @@ M.setup = function()
     hide = function(namespace, bufnr)
       local ns = vim.diagnostic.get_namespace(namespace)
       if ns.user_data.virt_lines_ns then
-        render.hide(ns.user_data.virt_lines_ns, bufnr )
+        render.hide(ns.user_data.virt_lines_ns, bufnr)
         vim.api.nvim_clear_autocmds({ group = "LspLines" })
       end
     end,
